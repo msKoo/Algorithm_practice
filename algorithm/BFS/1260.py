301244f81fce73aff2070842bf474c0cba56579c
msKoo <48276595+msKoo@users.noreply.github.com>
2022-02-15 10:38:56 +0000

    1260 _ input

---
+++ algorithm/BFS/1260.py
@@ -0,0 +1,12 @@
+########## 입력
+# print('입력하세요')
+N, M, K = map(int, input().split())
+
+num = []
+for m in range(M):
+  num.append(list(map(int, input().split())))
+########## 
+
+########### 내 답안
+
+##########
